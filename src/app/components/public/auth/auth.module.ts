2293c09c0949a7c120ebb9ec6221513c6fb0ef04
AndrewAK1619 <andrzej.kaminski0591@gmail.com>
2021-10-31 13:10:07 +0100

    011.0 - (Importy) Tworzenie fomularzy (formGroup i formly)

--- src/app/components/public/auth/auth.module.ts
+++ src/app/components/public/auth/auth.module.ts
@@ -1,9 +1,16 @@
+import { MatButtonModule } from '@angular/material/button';
+import { FormlyMaterialModule } from '@ngx-formly/material';
+import { FormlyModule } from '@ngx-formly/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth/auth.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { MatInputModule } from '@angular/material/input';
+import { FormlyMatDatepickerModule } from '@ngx-formly/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 const routes: Routes = [
   {
@@ -20,6 +27,7 @@ const routes: Routes = [
       }
     ]
   }
+  // --- Poniżej przykład dodatkowo z importem dzieci ---
   // ,
   // {
   //   path: 'prefix-link',
@@ -35,8 +43,14 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    ReactiveFormsModule, // będziemy wykorzystywać formGroupa z reaktywnymi formularzami
+    FormlyModule, // będziemy generować formularze za pmocą formly
+    FormlyMaterialModule, // korzystając z formly będziemy dociągać kontrolki materialowe
+    MatInputModule, // kontrolki do imputów
+    MatButtonModule, // kontrolki do buttonów
+    FormlyMatDatepickerModule,  // w formly datepicker
+    MatNativeDateModule, // w formly datepicker
   ]
 })
 export class AuthModule { }
