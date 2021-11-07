ed81be5241d7b41fa287ad7ddb03e21fe1711b40
AndrewAK1619 <andrzej.kaminski0591@gmail.com>
2021-11-07 12:00:47 +0100

    011.2 - (Login) -Tworzenie fomularzy (formGroup i formly)

--- src/app/components/public/auth/login/login.component.ts
+++ src/app/components/public/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  formGroup = new FormGroup({})
+  formFields: FormlyFieldConfig[] = [
+    {
+      key: "email",
+      type: "input",
+      templateOptions: {
+        label: "Email",
+        placeholder: "Enter email",
+        required: true,
+        maxLength: 255
+      },
+      validation: {
+        messages: {
+          required: (error, field) => "Value is required",
+          maxLength: (error, field) => "Lenght should be less then 255"
+        }
+      }
+    },
+    {
+      key: "password",
+      type: "input",
+      templateOptions: {
+        label: "Password",
+        placeholder: "Enter password",
+        required: true,
+        maxLength: 255
+      },
+      validation: {
+        messages: {
+          required: (error, field) => "Value is required",
+          maxLength: (error, field) => "Lenght should be less then 255"
+        }
+      }
+    },
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
