c5fdb962484857304704021856f05af97e9bfeeb
AndrewAK1619 <andrzej.kaminski0591@gmail.com>
2022-01-02 12:56:30 +0100

    031 - Dodanie wyświetlania produktu w public

--- src/app/components/public/product/product.module.ts
+++ src/app/components/public/product/product.module.ts
@@ -1,11 +1,34 @@
+import { BasicModule } from './../../basic/basic.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductComponent,
+    children: [
+      {
+        path: 'list',
+        component: ProductListComponent
+      }
+    ]
+  }
+]
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductComponent,
+    ProductListComponent,
+    ProductDetailsComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    BasicModule
   ]
 })
 export class ProductModule { }
