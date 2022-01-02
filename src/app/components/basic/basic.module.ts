97bcf715c32ad4a64260f6ec019bd06ff5fc0da3
AndrewAK1619 <andrzej.kaminski0591@gmail.com>
2022-01-02 12:28:41 +0100

    030 - Dodanie wszystkich kolumn do products i stylizacja

--- src/app/components/basic/basic.module.ts
+++ src/app/components/basic/basic.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products/products.component';
 import { NgxsModule } from '@ngxs/store';
 import { ProductState } from './state/product.state';
-
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
 
 
 @NgModule({
@@ -13,6 +14,8 @@ import { ProductState } from './state/product.state';
   imports: [
     CommonModule,
     NgxsModule.forFeature([ProductState]),
+    MatPaginatorModule,
+    MatTableModule
   ],
   exports: [
     ProductsComponent
