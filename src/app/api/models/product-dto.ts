0316cbf755145a64b8a682e40ac4c2f997105da8
AndrewAK1619 <andrzej.kaminski0591@gmail.com>
2022-01-09 13:36:17 +0100

    033 - Generowanie swaggera

--- src/app/api/models/product-dto.ts
+++ src/app/api/models/product-dto.ts
@@ -1,7 +1,11 @@
 /* tslint:disable */
 export interface ProductDto {
+  createdBy?: string;
+  createdDate?: string;
   description?: string;
   id?: number;
+  lastModifiedBy?: string;
+  lastModifiedDate?: string;
   name?: string;
   price?: number;
   quantity?: number;
--- src/app/api/models/user-dto.ts
+++ src/app/api/models/user-dto.ts
@@ -2,10 +2,15 @@
 export interface UserDto {
   birthDate?: string;
   confirmPassword?: string;
+  createdBy?: string;
+  createdDate?: string;
   email?: string;
   firstName?: string;
   id?: number;
+  lastModifiedBy?: string;
+  lastModifiedDate?: string;
   lastName?: string;
   password?: string;
   revisionNumber?: number;
+  roles?: Array<string>;
 }
